7eb457ca3358a85fb297a30cb623fad13ae7873e
Zach Nash <afreakenturtle@gmail.com>
2024-05-22 13:56:00 -0400

    moved user interface outside the function, fixed the use state hook

--- src/Components/UserSearch.tsx
+++ src/Components/UserSearch.tsx
@@ -1,21 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
+import React, { useState, useEffect } from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
-
-const UserSearch = () => {
-let [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(data => setUsers(data))
-    }, [])
-    return (
-        <>
-        <h1>Hello World</h1>
-        </>
-    );
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  };
 }
 
-export default UserSearch
+const UserSearch = () => {
+    let [users, setUsers] = useState<User[]>([]);
+
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <Autocomplete
+      disablePortal
+      id="user-search"
+      options={users}
+      sx={{ width: 300 }}
+      renderInput={(params) => <TextField {...params} label="Users" />}
+    />
+  );
+};
+
+export default UserSearch;
